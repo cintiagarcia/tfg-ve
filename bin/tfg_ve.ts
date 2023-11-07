23ebe41c0ca2afd99d75219772d6970b0d724cb1
I559673 <cintia.garcia.garces@sap.com>
2023-11-07 15:20:01 +0100

    feat: add kmsKey || create s3 bucket and conf directory

--- bin/tfg_ve.ts
+++ bin/tfg_ve.ts
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import * as fs from 'fs';
 import { TfgVeStack } from '../lib/tfg_ve-stack';
 
 const app = new cdk.App();
-new TfgVeStack(app, 'TfgVeStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+//get context params from the CDK application
+const env = 'tfg-ve-env';
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+//get context from config 
+const context = JSON.parse(fs.readFileSync('./conf/env.json', 'utf-8'));
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+//retrieve the account and region to your CDK stack
+const account = context[env].account;
+const region = context[env].region;
+
+new TfgVeStack(app, 'tfg-ve-stack', {
+  //envName: env,
+  env: { 
+    account: account,
+    region: region
+  }
 });
